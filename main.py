8fa652f3e4cc47759678e3e70c3d4445c96dc7f8
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2022-03-21 08:33:56 -0400

    Add requirements.txt, initialize LCD, use questions from USB if exists

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import csv
+import os
+
+import adafruit_character_lcd.character_lcd_i2c as character_lcd
+import board
+import busio
 
 from Question import *
 
 if __name__ == '__main__':
+    # LCD initialization
+    i2c = busio.I2C(board.SCL, board.SDA)
+    lcd = character_lcd.Character_LCD_I2C(i2c, 20, 4)
+    lcd.message = "Hello world!"
+
+    # Question initialization
     questions: set[Question] = set()
 
-    with open("test_questions.csv") as csvfile:
+    BASE_USB_PATH = "/media/pi/questionusb"
+    with open((BASE_USB_PATH + "/questions.csv") if os.path.isdir(BASE_USB_PATH) else "test_questions.csv") as csvfile:
         csv_reader = csv.reader(csvfile)
         for row in csv_reader:
             qstring = row[0]
