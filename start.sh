aca303fbc26ff688e6d4c0ad3e7e5f5e58c54b89
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2022-03-24 17:32:59 -0400

    Disable pip install

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 cd /home/pi/trivia_dispenser
 git pull
 source venv/bin/activate
-pip install -r requirements.txt
+# pip install -r requirements.txt
 echo "Starting Python script"
 python main.py
 #journalctl -e --user-unit trivia-dispenser.service >> /media/usb/test
