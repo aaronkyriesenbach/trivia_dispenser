8632602a49e57ee1781f9959304a626fc747b3a6
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2022-03-24 17:37:47 -0400

    Initialize I2C interface

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     print("Script started")
 
     # LCD initialization
-    # i2c = board.I2C()
+    i2c = board.I2C()
     # lcd = character_lcd.Character_LCD_I2C(i2c, 20, 4, address=0x27)
     # lcd.backlight = True
     # lcd.message = "Hello world!"
