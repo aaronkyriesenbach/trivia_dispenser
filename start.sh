de85bebd89385af1253488e64ed01b4f64a17500
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2022-04-04 23:07:09 -0400

    Wait for network before pull

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 cd /home/pi/trivia_dispenser
+
+while ! ping -c 1 github.com; do
+  echo "Waiting for network"
+  sleep 1
+done
+
 git pull
 source venv/bin/activate
 # pip install -r requirements.txt
