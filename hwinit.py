8d979fe23c9c7ecb6ea7366244e1798d127065c3
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2022-04-09 15:59:32 -0400

    Break hardware initialization out to separate file, add get_pressed_key()

---
+++ hwinit.py
@@ -0,0 +1,30 @@
+import adafruit_character_lcd.character_lcd as characterlcd
+import adafruit_matrixkeypad
+import board
+import digitalio
+
+PIN_RS = digitalio.DigitalInOut(board.D2)
+PIN_EN = digitalio.DigitalInOut(board.D3)
+PIN_D4 = digitalio.DigitalInOut(board.D4)
+PIN_D5 = digitalio.DigitalInOut(board.D17)
+PIN_D6 = digitalio.DigitalInOut(board.D27)
+PIN_D7 = digitalio.DigitalInOut(board.D22)
+
+COLS = 20
+ROWS = 4
+
+
+def init_lcd():
+    return characterlcd.Character_LCD_Mono(PIN_RS, PIN_EN, PIN_D4, PIN_D5, PIN_D6, PIN_D7, COLS, ROWS)
+
+
+cols = [digitalio.DigitalInOut(x) for x in (board.D5, board.D6, board.D13, board.D19)]
+rows = [digitalio.DigitalInOut(x) for x in (board.D26, board.D16, board.D20, board.D21)]
+keys = [(1, 2, 3, "A"),
+        (4, 5, 6, "B"),
+        (7, 8, 9, "C"),
+        (0, "F", "E", "D")]
+
+
+def init_keypad():
+    return adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
--- main.py
+++ main.py
@@ -1,35 +1,22 @@
-import adafruit_character_lcd.character_lcd as characterlcd
-import adafruit_matrixkeypad
-import board
-import digitalio
+from hwinit import *
 
-PIN_RS = digitalio.DigitalInOut(board.D2)
-PIN_EN = digitalio.DigitalInOut(board.D3)
-PIN_D4 = digitalio.DigitalInOut(board.D4)
-PIN_D5 = digitalio.DigitalInOut(board.D17)
-PIN_D6 = digitalio.DigitalInOut(board.D27)
-PIN_D7 = digitalio.DigitalInOut(board.D22)
+# Hardware initialization
+lcd = init_lcd()
+keypad = init_keypad()
+
+
+def get_pressed_key(pressed_keys):
+    return ''.join(str(k) for k in pressed_keys)
+
 
 if __name__ == '__main__':
-    # LCD initialization
-    lcd = characterlcd.Character_LCD_Mono(PIN_RS, PIN_EN, PIN_D4, PIN_D5, PIN_D6, PIN_D7, 20, 4)
-
     lcd.message = "Hello world!"
-
-    # Keypad initialization
-    cols = [digitalio.DigitalInOut(x) for x in (board.D5, board.D6, board.D13, board.D19)]
-    rows = [digitalio.DigitalInOut(x) for x in (board.D26, board.D16, board.D20, board.D21)]
-    keys = [(1, 2, 3, "A"),
-            (4, 5, 6, "B"),
-            (7, 8, 9, "C"),
-            (0, "F", "E", "D")]
-    keypad = adafruit_matrixkeypad.Matrix_Keypad(rows, cols, keys)
 
     while True:
         keys = keypad.pressed_keys
         if keys:
             lcd.clear()
-            lcd.message = ''.join(str(k) for k in keys)
+            lcd.message = get_pressed_key(keys)
 
     # Question initialization
     questions: set[Question] = set()
